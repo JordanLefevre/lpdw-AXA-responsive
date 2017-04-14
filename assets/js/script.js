7b5d499eea27c8d38b97d75d10fb8c95ec16f574
Florian Rambur <flo.rambur@gmail.com>
2017-04-14 16:14:19 +0200

    Docs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,86 @@
-var cross = document.getElementById('cross_for_entretien');
-var extend = document.getElementById('extend');
 var selectAllCheckbox = document.getElementById('selectAllCheckbox');
 
 var inputsChecked = 0;
-var subMenu = document.getElementById('subMenu');
-var isChecked = [];
+var subMenu = document.querySelectorAll('.subMenu');
+
+var size = document.querySelectorAll(".each_doc input");
+var displaySize = document.querySelector(".button_link");
+var totalSize = 0;
+
+for (var i = 0; i < size.length; i++) {
+    size[i].addEventListener('change', function() {
+        if (this.checked) {
+            totalSize += parseInt(this.nextElementSibling.querySelector('p').dataset.size);
+        } else {
+            totalSize -= parseInt(this.nextElementSibling.querySelector('p').dataset.size);
+        }
+        displaySize.value = "Télécharger " + totalSize/1000 + "Mo";
+    });
+}
 
 if (subMenu) {
-    var inputs = subMenu.getElementsByTagName('input');
-    for (var i = 0; i < inputs.length; i++) {
-        inputs[i].addEventListener('change', function(e) {
-            if (this.checked) {
-                isChecked.push(this);
-            } else {
-                isChecked.splice(isChecked.indexOf(this), 1);
-            }
+    for (var i = 0; i < subMenu.length; i++) {
 
-            if (isChecked.length == inputs.length) {
-                cross.style.visibility = "visible";
-                extend.style.visibility = "hidden";
-            } else if (isChecked.length == 0) {
-                cross.style.visibility = "hidden";
-                extend.style.visibility = "hidden";
-            } else {
-                extend.style.visibility = "visible";
-                cross.style.visibility = "hidden";
-            }
-
-        });
-
-        function selectAll() {
-            if (isChecked.length == 0) {
-                for (var i = 0; i < inputs.length; i++) {
-                    inputs[i].checked = true;
-                    cross.style.visibility = "visible";
-                    extend.style.visibility = "hidden";
-                    isChecked.push(this);
-                }
-                console.log(isChecked.length);
-                console.log(inputs.length);
-            } else if (isChecked.length == inputs.length) {
-                for (var i = 0; i < inputs.length; i++) {
-                    inputs[i].checked = false;
-                    cross.style.visibility = "hidden";
-                    extend.style.visibility = "hidden";
-                    isChecked = [];
+        for (var j = 0; j < subMenu[i].getElementsByTagName('input').length; j++) {
+            subMenu[i].getElementsByTagName('input')[j].addEventListener('change', function () {
+                if (this.closest('.subMenu').querySelectorAll('input:checked').length == this.closest('.subMenu').getElementsByTagName('input').length) {
+                    this.closest('.each_doc').querySelector('.cross').style.visibility = "visible";
+                    this.closest('.each_doc').querySelector('.extend').style.visibility = "hidden";
+                    this.closest('.each_doc').querySelector('.globalState').classList.toggle('checked');
+                    this.closest('.each_doc').querySelector('.globalState').classList.toggle('unchecked');
+                } else if (this.closest('.subMenu').querySelectorAll('input:checked').length == 0) {
+                    this.closest('.each_doc').querySelector('.cross').style.visibility = "hidden";
+                    this.closest('.each_doc').querySelector('.extend').style.visibility = "hidden";
+                    this.closest('.each_doc').querySelector('.globalState').classList.toggle('checked');
+                    this.closest('.each_doc').querySelector('.globalState').classList.toggle('unchecked');
+                } else {
+                    this.closest('.each_doc').querySelector('.extend').style.visibility = "visible";
+                    this.closest('.each_doc').querySelector('.cross').style.visibility = "hidden";
                 }
 
+            });
+
+            function selectAll(e) {
+                let spanState = e.target.closest('.globalState')
+                spanState.classList.toggle('checked');
+                spanState.classList.toggle('unchecked');
+
+                inputs = spanState.closest('.each_doc').querySelectorAll('.subMenu input');
+
+                if(spanState.classList.contains('checked')) {
+                    spanState.closest('.each_doc').querySelector('.cross').style.visibility = "visible";
+                } else {
+                    spanState.closest('.each_doc').querySelector('.cross').style.visibility = "hidden";
+                }
+
+                spanState.closest('.each_doc').querySelector('.extend').style.visibility = "hidden";
+
+                for(var i = 0; i < inputs.length; i++) {
+                    if(spanState.classList.contains('checked')) {
+                        inputs[i].checked = true;
+                    } else {
+                        inputs[i].checked = false;
+                    }
+                }
+
+                /*if (subMenu[i].querySelectorAll('input:checked').length == 0) {
+                    for (var j = 0; j < subMenu[i].getElementsByTagName('input').length; j++) {
+                        let input = subMenu[i].getElementsByTagName('input')[j];
+
+                        input.checked = true;
+                        input.closest('.each_doc').querySelector('.cross').style.visibility = "visible";
+                        input.closest('.each_doc').querySelector('.extend').style.visibility = "hidden";
+                    }
+                } else if (subMenu[i].querySelectorAll('input:checked').length == subMenu[i].getElementsByTagName('input').length) {
+                    for (var j = 0; j < subMenu[i].getElementsByTagName('input').length; j++) {
+                        let input = subMenu[i].getElementsByTagName('input')[j];
+
+                        input.checked = false;
+                        input.closest('.each_doc').querySelector('.cross').style.visibility = "hidden";
+                        input.closest('.each_doc').querySelector('.extend').style.visibility = "hidden";
+                    }
+
+                }*/
             }
         }
     }
